75338d9ff48ecff02111626be95ebab44ee814bd
danielJL19 <danieljmnz15@hotmail.com>
2023-06-19 12:55:31 -0400

    feat: add new images in page project and I add a form in page contact

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root 'pages#home'
+  get 'pages/project',as: :project
+  get 'pages/contact',as: :contact
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
